7a2cacf93583f3688dc8f7a81bb2fd522a838d76
Jacob Lucas <lucas.jacob.robert@gmail.com>
2019-06-06 17:57:34 -0400

    folder structure

--- run/flask_app/routes.py
+++ run/flask_app/routes.py
@@ -1,8 +1,9 @@
 from flask import jsonify, request
 import os
 from .run import app
-from flask_app import portfolio
+# from flask_app import portfolio
 
 @app.route('/')
 def landing():
-    return jsonify(portfolio())
+    test = {'this': 'works'}
+    return jsonify(test)
